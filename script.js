7b4a47731dface93d6e80ab944db9990b5884d32
mizaelaa <mizaelanunes@gmail.com>
2025-04-25 11:16:59 -0300

    Atualizações em HTML, CSS, JS e imagens do sistema de estacionamento

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 document.getElementById("reservaForm").addEventListener("submit", function (event) {
-    event.preventDefault();
-  
-    const formData = new FormData(this);
-    const reserva = {};
-    formData.forEach((value, key) => {
-      reserva[key] = value;
-    });
-  
-    console.log("Reserva cadastrada:", reserva);
-  
-    alert("Cadastro realizado com sucesso!");
-    this.reset(); // limpa o formulário
+  event.preventDefault();
+
+  const formData = new FormData(this);
+  const reserva = {};
+  formData.forEach((value, key) => {
+    reserva[key] = value;
   });
-  
+
+  // Salvar no localStorage
+  const reservas = JSON.parse(localStorage.getItem("reservas")) || [];
+  reservas.push(reserva);
+  localStorage.setItem("reservas", JSON.stringify(reservas));
+
+  console.log("Reserva cadastrada:", reserva);
+
+  alert("Cadastro realizado com sucesso!");
+  this.reset(); // limpa o formulário
+});
